40f4ab8729bb4885f96d51dec403ef9756c371ae
Umar Baig <Umarbaig2012.ub@gmail.com>
2019-11-12 12:40:30 +0500

    check
    check

---
+++ src/components/user-profile-lite/UserDetails.js
@@ -0,0 +1,77 @@
+import React from "react";
+import PropTypes from "prop-types";
+import {
+  Card,
+  CardHeader,
+  Button,
+  ListGroup,
+  ListGroupItem,
+  Progress
+} from "shards-react";
+
+
+const UserDetails = ({ userDetails }) => (
+  
+  <Card small className="mb-4 pt-3">
+    <CardHeader className="border-bottom text-center">
+      <div className="mb-3 mx-auto">
+        <img
+          className="rounded-circle"
+          src={userDetails.avatar}
+          alt={userDetails.name}
+          width="110"
+        />
+      </div>
+      <h4 className="mb-0">{userDetails.name}</h4>
+      <span className="text-muted d-block mb-2">{userDetails.jobTitle}</span>
+     {/* <Button pill outline size="sm" className="mb-2">
+        <i className="material-icons mr-1">person_add</i> Follow
+</Button>*/}
+    </CardHeader>
+    <ListGroup flush>
+      {/* <ListGroupItem className="px-4">
+       <div className="progress-wrapper">
+          <strong className="text-muted d-block mb-2">
+            {userDetails.performanceReportTitle}
+          </strong>
+          <Progress
+            className="progress-sm"
+            value={userDetails.performanceReportValue}
+          >
+            <span className="progress-value">
+              {userDetails.performanceReportValue}%
+            </span>
+          </Progress>
+</div>
+</ListGroupItem>*/}
+      <ListGroupItem className="p-4">
+        <strong className="text-muted d-block mb-2">
+          {userDetails.metaTitle}
+        </strong>
+        <span>{userDetails.metaValue}</span>
+      </ListGroupItem>
+    </ListGroup>
+  </Card>
+);
+
+UserDetails.propTypes = {
+  /**
+   * The user details object.
+   */
+  userDetails: PropTypes.object
+};
+
+UserDetails.defaultProps = {
+  userDetails: {
+    name: "Umar Baig",
+    avatar: "https://scontent-sin2-1.cdninstagram.com/vp/5c79bc6b72045b86798854f4f26f67d5/5E429BBB/t51.2885-19/s150x150/53396441_367497184097338_483845318778028032_n.jpg?_nc_ht=scontent-sin2-1.cdninstagram.com",
+    jobTitle: "10th Grade",
+    performanceReportTitle: "Workload",
+    performanceReportValue: 74,
+    metaTitle: "Description",
+    metaValue:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio eaque, quidem, commodi soluta qui quae minima obcaecati quod dolorum sint alias, possimus illum assumenda eligendi cumque?"
+  }
+};
+
+export default UserDetails;
---
+++ src/views/Signin.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Container, Row, Col } from "shards-react";
+
+import PageTitle from "../components/common/PageTitle";
+import UserDetails from "../components/user-profile-lite/UserDetails";
+import UserAccountDetails from "../components/user-profile-lite/UserAccountDetails";
+
+const Signin = () => (
+  <Container fluid className="main-content-container px-4">
+   
+  
+
+  </Container>
+);
+export default Signin;
